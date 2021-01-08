b6c1e49aad337ca7cc0fc01fc2b8ff2657a2ee35
mas-agis <53692594+mas-agis@users.noreply.github.com>
2021-01-08 15:57:55 +0100

    Update slurm-out_metadata.py

--- slurm-out_metadata.py
+++ slurm-out_metadata.py
@@ -17,32 +17,48 @@ df=pd.DataFrame(a)
 for i,k in enumerate(files):
     with open(k, 'r') as file:
         slurm_out = file.read().replace('\n', '')
+
+##where I am struggling now --- the idea is if slurm_out has 'BaseRecalibrator' then continue with analysis
+##otherwise skip to the next file
+    bqsr_Regex=re.compile(r'\bBaseRecalibrator\b')
+    bqsr=bqsr_Regex.search(slurm_out).group()                      
+    if bqsr=='BaseRecalibrator':
+        print("okay ada di " + str(k))
+    else:
+        print("nggak ketemu ee di" + str(k))
+        break
+
+for n in range(2, 10):
+...     for x in range(2, n):
+...         if n % x == 0:
+...             print(n, 'equals', x, '*', n//x)
+...             break
+...     else:
+...         # loop fell through without finding a factor
+...         print(n, 'is a prime number')
+    
+    if re.compile(r'(BaseRecalibrator -)
+##until here 
+                  
     #finding individual SRA    
     individuRegex=re.compile(r'\bSRR\d{7}') #creating regex pattern of SRA(boundary SRR followed by 7 digits)
     sample=individuRegex.search(slurm_out) #serch regex pattern in the slurm-out
-    print("individu in this file is " + sample.group())
+    #print("individu in this file is " + sample.group())
     df.loc[i,"SRA"]=sample.group()
     #finding clean reads
     clean_readsRegex=re.compile(r'(Processed) (\d+) (total reads)') #brackets automatically group the pattern
     clean_reads=clean_readsRegex.search(slurm_out).group(2) #search and take the second group of matching strings
-    print("clean reads are: " + clean_reads)
+    #print("clean reads are: " + clean_reads)
     df.loc[i,"clean_reads"]=int(clean_reads)
     #finding filtered reads
     filtered_readsRegex=re.compile(r'(BaseRecalibrator -) (\d+) (read\(s\) filtered by)')
     filtered_reads=filtered_readsRegex.search(slurm_out).group(2)
-    print("filtered reads are: " + filtered_reads)
+    #print("filtered reads are: " + filtered_reads)
     df.loc[i,"filtered_reads"]=int(filtered_reads)
     #finding refseq works!
     refseqRegex=re.compile(r'.{10}\.fa ') # | ARS_UCD1.2.fa  creating regex pattern of either brahman or ars refseq
     refseq=refseqRegex.search(slurm_out)
-    print("the refseq is " + refseq.group())
+    #print("the refseq is " + refseq.group())
     df.loc[i,"REF"]=refseq.group()
-    #trial
-    #refseqRegex=re.compile(r'UOA') #creating regex pattern of either brahman or ars refseq
-    #refseq=refseqRegex.search(slurm_out)
-    #print("the refseq is " + refseq.group(1))
-    #df.loc[i,"REF"]=refseq.group()    
-    #adding total reads
+    #total_reads 
     df.loc[i,"total_reads"]=df.loc[i,"filtered_reads"]+df.loc[i,"clean_reads"]
-##!!Notes: apparently total_reads above are the clean reads only, thus filtered reads must be added
-
