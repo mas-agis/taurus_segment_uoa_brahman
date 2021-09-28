593bf13a1d180ed4f78246bddeaa7c510a803d73
mas-agis <53692594+mas-agis@users.noreply.github.com>
2021-09-28 09:59:51 +0200

    Update ttest_snvs_total.R

--- ttest_snvs_total.R
+++ ttest_snvs_total.R
@@ -32,15 +32,12 @@ t.test(data$Map.ars, data$Map.uoa, paired = TRUE, alternative = "greater") #mapp
 t.test(data$Ret.ars, data$Ret.uoa, paired = TRUE, alternative = "greater") #retained reads
 
 ##################################################################################
-##prepared ggplot for admixture CV plot
+##ggplot for admixture CV plot
 library(dplyr)
 library(ggplot2)
-plt1 = c(7.5, 7.3, 7.1, 7.3, 7.6, 7.9)
-plt2 = plt1 +0.2 
-plt3 = plt1 + 0.13
-plt4 = plt1 - 0.135
-K = c(2, 3, 4, 5, 6, 7)
-data = data.frame(K=rep(2:7, 4), cv=c(plt1, plt2, plt3, plt4),
-                  SNVs=rep(paste0("plt", 1:4), each=6))
+#read data
+setwd("D:/maulana/GSE_second_paper/september_version/raw_images")
+list.files()
+data = read.table("CV_error_admixture.txt", header = TRUE)
 # plot
-ggplot(data = data, aes(x=K, y=cv)) + geom_line(aes(colour=SNVs))
+ggplot(data = data, aes(x=K, y=CV.error)) + geom_line(aes(colour=SNVs))
